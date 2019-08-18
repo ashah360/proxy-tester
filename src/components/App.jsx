05c43e5eac59bf040b6e961b13fc7e805142afe1
Arnav Malhotra <spideynuff@gmail.com>
2019-08-17 21:32:25 -0700

    Add support for React modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
 
+import Logo from '../assets/logo.svg';
 import USFlag from '../assets/us-flag.svg';
 import JPFlag from '../assets/jp-flag.svg';
 import USFlagFaded from '../assets/us-flag-faded.svg';
@@ -10,6 +10,8 @@ import SunDark from '../assets/sun-dark.svg';
 
 import LightTheme from '../styles/App-light.module.css';
 import DarkTheme from '../styles/App-dark.module.css';
+
+import '../styles/App.css';
 
 class App extends Component {
 	constructor() {
@@ -61,7 +63,7 @@ class App extends Component {
 		this.state = {
 			region: 'US',
 			theme: 'light',
-			cssText: {
+			themeCSS: {
 				light: LightTheme,
 				dark: DarkTheme
 			},
@@ -94,59 +96,60 @@ class App extends Component {
 	}
 
 	render() {
+		this.state.css = this.state.themeCSS[this.state.theme];
+
 		return (
-			<div className="container">
-				<style>{this.state.cssText[this.state.theme]}</style>
+			<div className={this.state.css['container']}>
 				<header>
-					<img id="logo" src={logo} />
+					<img id={this.state.css['logo']} src={Logo} />
 
-					<div id="icons">
-						<img id="us-flag" onClick={this.handleRegionChange} src={this.state.flags.US} alt="" />
-						<img id="jp-flag" onClick={this.handleRegionChange} src={this.state.flags.JP} alt="" />
+					<div id={this.state.css['icons']}>
+						<img id={this.state.css['us-flag']} onClick={this.handleRegionChange} src={this.state.flags.US} alt="" />
+						<img id={this.state.css['jp-flag']} onClick={this.handleRegionChange} src={this.state.flags.JP} alt="" />
 
-						<img id="divider" src={Divider} alt="" />
+						<img id={this.state.css['divider']} src={Divider} alt="" />
 
-						<img id="sun-dark" onClick={this.changeTheme} src={SunDark} alt="" />
+						<img id={this.state.css['sun-dark']} onClick={this.changeTheme} src={SunDark} alt="" />
 					</div>
 				</header>
 
-				<div className="form-control-group">
+				<div className={this.state.css['form-control-group']}>
 					<div style={{ display: 'inline-block', float: 'left', width: '74%' }}>
-						<label className="form-label" htmlFor="inputProxy">{this.state.proxyText[this.state.region]}</label>
-						<input id="inputProxy" type="text"></input>
+						<label className={this.state.css['form-label']} htmlFor="inputProxy">{this.state.proxyText[this.state.region]}</label>
+						<input className={this.state.css['input']} id="inputProxy" type="text"></input>
 					</div>
 
 					<div style={{ display: 'inline-block', float: 'right', width: '24%' }}>
-						<label className="form-label" htmlFor="inputPort">{this.state.portText[this.state.region]}</label>
-						<input id="inputPort" type="text"></input>
+						<label className={this.state.css['form-label']} htmlFor="inputPort">{this.state.portText[this.state.region]}</label>
+						<input className={this.state.css['input']} id="inputPort" type="text"></input>
 					</div>
 
 					<div style={{ display: 'inline-block', marginTop: '40px', float: 'left', width: '49%' }}>
-						<label className="form-label" htmlFor="inputUser">{this.state.usernameText[this.state.region]}</label>
-						<input id="inputUser" type="text"></input>
+						<label className={this.state.css['form-label']} htmlFor="inputUser">{this.state.usernameText[this.state.region]}</label>
+						<input className={this.state.css['input']} id="inputUser" type="text"></input>
 					</div>
 
 					<div style={{ display: 'inline-block', marginTop: '40px', float: 'right', width: '49%' }}>
-						<label className="form-label" htmlFor="inputPassword">{this.state.passwordText[this.state.region]}</label>
-						<input id="inputPassword" type="password"></input>
+						<label className={this.state.css['form-label']} htmlFor="inputPassword">{this.state.passwordText[this.state.region]}</label>
+						<input className={this.state.css['input']} id="inputPassword" type="password"></input>
 					</div>
 				</div>
 
-				<span id="cb-paste">{this.state.pasteText[this.state.region]}</span>
+				<span id={this.state.css['cb-paste']}>{this.state.pasteText[this.state.region]}</span>
 
-				<div className="form-control-group">
+				<div className={this.state.css['form-control-group']}>
 					<div style={{ display: 'inline-block', float: 'left', width: '74%' }}>
-						<label className="form-label" htmlFor="inputDomain">{this.state.domainText[this.state.region]}</label>
-						<input id="inputDomain" type="text"></input>
+						<label className={this.state.css['form-label']} htmlFor="inputDomain">{this.state.domainText[this.state.region]}</label>
+						<input className={this.state.css['input']} id="inputDomain" type="text"></input>
 					</div>
 
 					<div style={{ display: 'inline-block', float: 'right', width: '24%' }}>
-						<label className="form-label" htmlFor="inputPing">{this.state.pingText[this.state.region]}</label>
-						<input id="inputPing" type="text" value="203 ms" disabled></input>
+						<label className={this.state.css['form-label']} htmlFor="inputPing">{this.state.pingText[this.state.region]}</label>
+						<input className={this.state.css['input']} id="inputPing" type="text" value="203 ms" disabled></input>
 					</div>
 
-					<button style={{ marginTop: '15px' }} className="form-btn blue-btn">{this.state.pingTestText[this.state.region]}</button>
-					<button style={{ marginTop: '16px', marginBottom: '25px' }} className="form-btn grey-btn">{this.state.connectText[this.state.region]}</button>
+					<button style={{ marginTop: '15px' }} className={[this.state.css['form-btn'], this.state.css['blue-btn']].join(' ')}>{this.state.pingTestText[this.state.region]}</button>
+					<button style={{ marginTop: '16px', marginBottom: '25px' }} className={[this.state.css['form-btn'], this.state.css['grey-btn']].join(' ')}>{this.state.connectText[this.state.region]}</button>
 				</div>
 			</div>
 		);
